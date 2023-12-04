8525a025ba1409d8cbacb647000d1900bcc056f6
KelvinatorWas <liepkalnskalvis@gmail.com>
2023-12-04 14:31:57 +0200

    added clear form

--- src/components/main/cardForm.tsx
+++ src/components/main/cardForm.tsx
@@ -40,6 +40,7 @@ const CardForm = ({ createData }: TypeCardForm) => {
 
     createData(updatedFormData);
 
+    setFormData({...defaultData});
   };
 
   // yeah idk
@@ -47,31 +48,31 @@ const CardForm = ({ createData }: TypeCardForm) => {
     <div className="iid-input-box">
       <form id="iidForm" onSubmit={handleSubmit}>
         <label htmlFor="name" className="iid-input-text">Name:</label>
-        <input type="text" name="name" onChange={handleInputChange} required/><br/>
+        <input type="text" name="name" value={formData.name} onChange={handleInputChange} required/><br/>
 
         <label htmlFor="species" className="iid-input-text">Species:</label>
-        <input type="text" name="species" onChange={handleInputChange} required/><br/>
+        <input type="text" name="species" value={formData.species} onChange={handleInputChange} required/><br/>
 
         <label htmlFor="sex" className="iid-input-text">Sex:</label>
-        <input type="text" name="sex" onChange={handleInputChange} required/><br/>
+        <input type="text" name="sex" value={formData.sex} onChange={handleInputChange} required/><br/>
 
         <label htmlFor="origin" className="iid-input-text">Origin:</label>
-        <input type="text" name="origin" onChange={handleInputChange} required/><br/>
+        <input type="text" name="origin" value={formData.origin} onChange={handleInputChange} required/><br/>
 
         <label htmlFor="occupation" className="iid-input-text">Occupation:</label>
-        <input type="text" name="occupation" onChange={handleInputChange} required/><br/>
+        <input type="text" name="occupation" value={formData.occupation} onChange={handleInputChange} required/><br/>
 
         <label htmlFor="pfp" className="iid-input-text">Pfp:</label>
-        <input type="text" onChange={handleInputChange} name="pfp"/><br/>
+        <input type="text" value={formData.pfp} onChange={handleInputChange} name="pfp"/><br/>
 
         <label htmlFor="birth_year" className="iid-input-text">Birth Year:</label>
-        <input type="text" name="birth_year" onChange={handleInputChange} required/><br/>
+        <input type="text" name="birth_year" value={formData.birth_year} onChange={handleInputChange} required/><br/>
 
         <label htmlFor="status" className="iid-input-text">Status:</label>
-        <input type="text" name="status" onChange={handleInputChange} /><br/>
+        <input type="text" name="status" value={formData.status} onChange={handleInputChange} /><br/>
 
         <label htmlFor="desc" className="iid-input-text">Description:</label><br/>
-        <textarea name="desc" rows={4} onChange={handleInputChange}/><br/>
+        <textarea name="desc" rows={4} value={formData.desc}onChange={handleInputChange}/><br/>
 
         <button className="submit-button js-submit-button" type="submit">Submit</button>
       </form>
